50af22c106d35e1e26370540386f2ceb2e67c2b1
zhewuzhou <zhewuzhou@gmail.com>
2020-09-18 22:24:46 +0800

    [Z.Z][Pattern] extract constants & methods

--- src/test/java/zhewu/me/code/patterns/before/conditional/LoanTest.java
+++ src/test/java/zhewu/me/code/patterns/before/conditional/LoanTest.java
@@ -1,8 +1,10 @@
 package zhewu.me.code.patterns.before.conditional;
 
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.closeTo;
@@ -18,13 +20,23 @@ class LoanTest {
     @Test
     void capitalShouldWorksForTermLoan() {
         Loan termLoan = Loan.createTermLoan(COMMITMENT, START, MATURITY, RISK_RATING);
-
-        for (int i = 1; i <= 12; i++) {
-            termLoan.addPayment(PAYMENT_AMOUNT, LocalDateTime.of(2019, i, 15, 0, 0));
+        ArrayList<Payment> payments = createPayments();
+        for (int i = 0; i < 12; i++) {
+            termLoan.addPayment(payments.get(i).getAmount(), payments.get(i).getDate());
         }
 
         Double result = termLoan.capital();
 
         assertThat(result, closeTo(1483.5, 0.2));
+    }
+
+    @NotNull
+    private ArrayList<Payment> createPayments() {
+        ArrayList<Payment> payments = new ArrayList<>();
+        for (int i = 1; i <= 12; i++) {
+            payments.add(new Payment(PAYMENT_AMOUNT,
+                LocalDateTime.of(2019, i, 15, 0, 0)));
+        }
+        return payments;
     }
 }
